f36fb158a90934930f0c9a618f20dea72d9bfd26
Betuah Anugerah <betuahanugerah@gmail.com>
2022-09-28 16:29:02 +0700

    change api course to catalog

--- routes/course_route.js
+++ routes/course_route.js
@@ -2,15 +2,20 @@ const CourseController = require("../controllers/course");
 const { uploadToS3, uploadTmp } = require("../middlewares/uploadFiles");
 
 module.exports = (app) => {
-   app.route("/api/v1/course").get(CourseController.index);
+   app.route("/api/v1/course-catalog").get(CourseController.index);
 
-   app.route("/api/v1/course/:courseId").get(CourseController.getById);
+   app.route("/api/v1/course-catalog/:courseId").get(CourseController.getById);
 
-   app.route("/api/v1/course").post(uploadToS3, CourseController.create);
+   app.route("/api/v1/course-catalog").post(
+      uploadToS3,
+      CourseController.create
+   );
 
-   app.route("/api/v1/course").put(uploadTmp, CourseController.update);
+   app.route("/api/v1/course-catalog").put(uploadTmp, CourseController.update);
 
-   app.route("/api/v1/course/:courseId").delete(CourseController.delete);
+   app.route("/api/v1/course-catalog/:courseId").delete(
+      CourseController.delete
+   );
 
    app.route("/").get(CourseController.check);
 };
